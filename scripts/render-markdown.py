b76b221f9915dae0007700050d7ddb19db8172df
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2024-05-08 10:55:57 +0000

    chore: format python code with ruff

--- scripts/render-markdown.py
+++ scripts/render-markdown.py
@@ -1,19 +1,18 @@
 import sys
-import re
 import json
 
-header ='''# Anicca AOSC
+header = """# Anicca AOSC
 This is a small utility to run `aosc-findupdate` regularly by GitHub Actions.
-## Result'''
+## Result"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(header)
-    print('| Package | Repo Version | New Version | Path |')
-    print('|---------|--------------|-------------|------|')
+    print("| Package | Repo Version | New Version | Path |")
+    print("|---------|--------------|-------------|------|")
 
     table = json.loads(sys.stdin.read())
 
-    for row in sorted(table, key = lambda x: x["name"]):
-        row["before"] = row["before"].replace('+', '<br>+')
-        
-        print('|' + '|'.join(row.values()) + '|')
+    for row in sorted(table, key=lambda x: x["name"]):
+        row["before"] = row["before"].replace("+", "<br>+")
+
+        print("|" + "|".join(row.values()) + "|")
