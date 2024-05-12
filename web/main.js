1a36d23022a21b43725a59bc9fdfbe0bf554ffe9
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2024-05-12 06:40:34 +0000

    feat: add update time to the website

--- web/main.js
+++ web/main.js
@@ -1,40 +1,57 @@
-const dataUrl =
-  "https://raw.githubusercontent.com/Suyun114/anicca-aosc/main/pkgsupdate.json";
+async function renderDataTable() {
+  const dataUrl =
+    "https://raw.githubusercontent.com/Suyun114/anicca-aosc/main/pkgsupdate.json";
 
-const data = await fetch(dataUrl).then((response) => response.json());
+  const data = await fetch(dataUrl).then((response) => response.json());
 
-const dataSet = data.map((row) => {
-  row.before = row.before.replaceAll("+", "<br>+");
-  row.path = row.path.slice(0, row.path.lastIndexOf("/"));
-  return Object.values(row);
-});
+  const dataSet = data.map((row) => {
+    row.before = row.before.replaceAll("+", "<br>+");
+    row.path = row.path.slice(0, row.path.lastIndexOf("/"));
+    return Object.values(row);
+  });
 
-new DataTable("#pkgsupdate", {
-  columns: [
-    { title: "Package" },
-    { title: "Repo Version" },
-    { title: "New Version" },
-    { title: "Category" },
-    { title: "Warnings" },
-  ],
-  scrollX: true,
-  lengthMenu: [
-    10,
-    25,
-    50,
-    100,
-    { label: "8d", value: 0x8d },
-    { label: "All", value: -1 },
-  ],
-  search: {
-    regex: true,
-  },
-  layout: {
-    bottomEnd: {
-      paging: {
-        type: "simple_numbers",
+  new DataTable("#pkgsupdate", {
+    columns: [
+      { title: "Package" },
+      { title: "Repo Version" },
+      { title: "New Version" },
+      { title: "Category" },
+      { title: "Warnings" },
+    ],
+    scrollX: true,
+    lengthMenu: [
+      10,
+      25,
+      50,
+      100,
+      { label: "8d", value: 0x8d },
+      { label: "All", value: -1 },
+    ],
+    search: {
+      regex: true,
+    },
+    layout: {
+      bottomEnd: {
+        paging: {
+          type: "simple_numbers",
+        },
       },
     },
-  },
-  data: dataSet,
-});
+    data: dataSet,
+  });
+}
+
+async function getUpdateDate() {
+  const dataUrl =
+    "https://api.github.com/repos/Suyun114/anicca-aosc/branches/main";
+  const data = await fetch(dataUrl).then((response) => response.json());
+  const updateTimestamp = Date.parse(data.commit.commit.author.date);
+
+  const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+  document.getElementById("update-time").innerText =
+    "Updated " +
+    rtf.format(parseInt((updateTimestamp - Date.now()) / 60000), "minute");
+}
+
+renderDataTable();
+getUpdateDate();
